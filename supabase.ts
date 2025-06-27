8b7396c01572a2488be8f5a20edd2e91a20cad33
jakub92001 <46622367+jakub92001@users.noreply.github.com>
2025-06-27 23:44:38 +0200

    Update supabase.ts

--- supabase.ts
+++ supabase.ts
@@ -1,4 +1,5 @@
 
+
 import { createClient, SupabaseClient } from '@supabase/supabase-js';
 
 let supabase: SupabaseClient | null = null;
@@ -7,8 +8,8 @@ let supabaseError: string | null = null;
 try {
     // In Vite, client-side environment variables are exposed on `import.meta.env`
     // and must be prefixed with `VITE_` by default.
-    const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-    const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+    const supabaseUrl = (import.meta as any).env.VITE_SUPABASE_URL;
+    const supabaseAnonKey = (import.meta as any).env.VITE_SUPABASE_ANON_KEY;
 
     if (!supabaseUrl || !supabaseAnonKey) {
         throw new Error("Konfiguracja Supabase jest niekompletna. Upewnij się, że zmienne środowiskowe VITE_SUPABASE_URL i VITE_SUPABASE_ANON_KEY są ustawione.");
